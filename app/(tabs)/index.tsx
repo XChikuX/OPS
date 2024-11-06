815107af4c598ed5a66caa29f956779302208710
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2024-11-07 00:09:30 +0530

    [WIP] Fix asset-registry having multiple versions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import {Image, View, Platform } from 'react-native';
+import {View, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+
+import { Image } from 'expo-image';
 
 import LikeIcon from '@/assets/images/svg/like.svg';
 
