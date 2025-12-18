3eb7acc1440bb37fbbf9472c0e6bc928442940c8
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2025-12-18 15:43:32 +0530

    Better UI with Nativewind v5

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { withNativewind } = require('nativewind/metro');
+
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
+const config = getDefaultConfig(__dirname);
 
 config.transformer = {
   ...config.transformer,
@@ -22,4 +23,4 @@ config.resolver = {
   sourceExts: [...config.resolver.sourceExts, 'svg', 'd.ts', 'mjs'],
 };
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withNativewind(config);
